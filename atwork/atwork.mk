3b57afd3b2f14f42d40180221e4c77c5e049e52e
moriarty <alexander@dal.ca>
2016-05-06 01:05:55 +0200

    atwork: git filter-branch from rockin into atwork

---
+++ atwork/atwork.mk
@@ -0,0 +1,14 @@
+#*****************************************************************************
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#*****************************************************************************
+
+ifndef __buildsys_config_mk_
+$(error config.mk must be included before base.mk)
+endif
+
+CFLAGS += -I$(BASEDIR)/atwork
